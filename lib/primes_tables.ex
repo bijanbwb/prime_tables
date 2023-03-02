b65b1415161eb02e8f4e3150fa86af64afba972b
Bijan <bijanbwb@gmail.com>
2023-03-02 08:29:22 -0500

    Initialize new Mix project

---
+++ lib/primes_tables.ex
@@ -0,0 +1,18 @@
+defmodule PrimesTables do
+  @moduledoc """
+  Documentation for `PrimesTables`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrimesTables.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/primes_tables_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimesTablesTest do
+  use ExUnit.Case
+  doctest PrimesTables
+
+  test "greets the world" do
+    assert PrimesTables.hello() == :world
+  end
+end
