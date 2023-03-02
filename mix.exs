ff684946a1d5823b6fcb7125038daf4c3b1164ba
Bijan <bijanbwb@gmail.com>
2023-03-02 12:28:58 -0500

    Add dialyxir to verify typespecs are valid

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule PrimesTables.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
