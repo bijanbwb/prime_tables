b8516cd36f0ad71268596d2b427eed512596c344
Bijan <bijanbwb@gmail.com>
2023-03-02 08:47:04 -0500

    Scaffold out function to find primes with default case

--- lib/primes_tables.ex
+++ lib/primes_tables.ex
@@ -1,6 +1,11 @@
 defmodule PrimesTables do
   @moduledoc """
-  Documentation for `PrimesTables`.
+  This application is intended to gather a list of prime numbers from `2` to
+  an input value `number`.
+
+  Then, we use those prime numbers to generate a string output with
+  multiplication tables for those numbers.
+
   """
 
   #
@@ -14,12 +19,15 @@ defmodule PrimesTables do
   # Run
   #
 
-  @spec run(input :: input()) :: output()
-  def run(input) when is_integer(input) and input > 1 do
+  @spec run(number :: input()) :: output()
+  def run(number) when is_integer(number) and number > 1 do
     "output"
   end
 
-  def run(input) do
-    {:error, "The input value #{input} should be a whole number greater than 1."}
+  def run(number) do
+    {:error, "The input value #{number} should be a whole number greater than 1."}
   end
+
+  @spec find_primes(list(non_neg_integer())) :: list(non_neg_integer())
+  def find_primes([] = _numbers), do: []
 end
--- test/primes_tables_test.exs
+++ test/primes_tables_test.exs
@@ -2,10 +2,16 @@ defmodule PrimesTablesTest do
   use ExUnit.Case
   doctest PrimesTables
 
-  describe "run" do
+  describe "run/1" do
     test "provides errors for invalid input values" do
       assert {:error, _error_message} = PrimesTables.run(0)
       assert {:error, _error_message} = PrimesTables.run(1.5)
+    end
+  end
+
+  describe "find_primes/1" do
+    test "finds no primes for an empty list of numbers" do
+      assert [] == PrimesTables.find_primes([])
     end
   end
 end
